070c121a27a399e6e3be53ba87f1b2d3902de70b
Juan Diaz <juanferreira65@gmail.com>
2021-08-09 09:21:19 -0400

    email and footer finished

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ for( let i = 0; i < options.length ; i++ ){
 }
 
 
-// funcionalidades de FAQ
+// funcionalidades de FAQ agregar colapso en los demas links
 
 let faqs = document.querySelector(".faqs");
 let ask_container = faqs.getElementsByClassName("ask__container");
@@ -64,6 +64,14 @@ let arrow = faqs.getElementsByClassName("ask__arrow");
 for( let i = 0; i < ask_container.length ; i++ ){
 
     ask_container[i].addEventListener("click" , ()=>{
+        
+        for( let j = 0; j < ask_container.length; j++ ){
+
+            if( ask_container[j].classList.contains("active") && j != i ){
+                ask_container[j].classList.toggle("active");
+            }
+        }
+
         ask_container[i].classList.toggle("active");
         
         if ( !ask_container[i].classList.contains("active") ){
@@ -72,4 +80,46 @@ for( let i = 0; i < ask_container.length ; i++ ){
 
     });
 }
+
+//funcionalidades del input email
+
+let form = document.querySelector('form');
+    form.addEventListener( 'invalid',function(event){
+        event.preventDefault();
+    },true);
+
+let email = form.querySelector(".email__input");
+
+email.addEventListener("invalid" , ()=>{
+    
+    if( form.querySelector(".error") ){
+        let error = form.querySelector(".error");
+        form.removeChild(error);
+    }
+
+    let error = document.createElement("div");
+
+    error.textContent = "Whoops, make sure it's an email";
+
+    error.className = "error";
+
+    let submit = form.querySelector(".email__submit");
+
+    form.insertBefore(error,submit);
+
+    email.style.cssText = "border: 2px solid var(--soft-red);";
+
+    window.setTimeout(function(){
+
+        if( form.querySelector(".error") ){
+            let error = form.querySelector(".error");
+            form.removeChild(error);
+        }
+
+        email.style.cssText = "";
+    
+    },5000);
+});
+
+
 
